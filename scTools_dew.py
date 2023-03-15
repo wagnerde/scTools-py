c57824446b0168518fb31fd5981ee776aa45eb67
Dan Wagner <wagnerde@gmail.com>
2023-03-14 22:21:53 -0700

    Update scTools_dew.py

--- scTools_dew.py
+++ scTools_dew.py
@@ -1124,7 +1124,7 @@ def get_significant_pcs(adata, n_iter = 1, n_comps_test = 100, threshold_method=
     data_rand_max = []
     nPCs_above_rand = []
     for j in range(n_iter):
-        sys.stdout.write('\rIteration %i / %i' % (j, n_iter)); sys.stdout.flush()
+        sys.stdout.write('\rIteration %i / %i' % (j+1, n_iter)); sys.stdout.flush()
         #print('Iteration', j+1, '/', n_iter, end='\r')
         np.random.seed(seed=j)
         adata_tmp_rand = adata_tmp.copy()
